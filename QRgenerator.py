bd5428a8d47f47d40f45072ac5872626bf6bdc46
Christian Payne <67926222+ChrisPayneHome@users.noreply.github.com>
2020-08-14 00:02:01 +0100

    Update QRgenerator.py

--- QRgenerator.py
+++ QRgenerator.py
@@ -1,6 +1,22 @@
 import qrcode
+import time
 
-data = input("Please paste the url you would like to generate a QR code for:\n")
-filename = "QR.png"
-img = qrcode.make(data)
-img.save(filename)
+data = input("Hello, please enter the url that you would like your QR code to link to: \n")
+print("You entered %s" % data)
+time.sleep(1)
+print("Beginning process now...")
+time.sleep(1)
+
+qr = qrcode.QRCode(
+	version=1, 
+	error_correction=qrcode.constants.ERROR_CORRECT_L,
+	box_size=10,
+	border=4,
+)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+print("Done!")
